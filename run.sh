78dd7b9c531e121384fa04cea68e83cf8d30ecc3
Chris Sim <chriss@airg.com>
2016-06-09 16:57:17 -0700

    using system uWSGI to run, uwsgi.ini tweak for testing

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for line in $( cat /etc/environment ) ; do export $line ; done
-exec /airg/overwatchstats/localpy/bin/uwsgi --ini /airg/overwatchstats/uwsgi.ini
+exec /usr/bin/uwsgi --ini /airg/overwatchstats/uwsgi.ini
 
