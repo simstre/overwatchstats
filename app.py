ea6fa63c7de7c8655412d168ca783404328e16b6
Chris Sim <imchrissim@gmail.com>
2016-06-25 21:59:35 -0700

    blizzard's image hosting hostname changed

--- app.py
+++ app.py
@@ -56,59 +56,64 @@ def refresh():
     :return:
     """
     for player in players:
-        response = requests.get(overwatch_url.format(player[1]['handle']))
+        try:
+            response = requests.get(overwatch_url.format(player[1]['handle']))
 
-        # Scrapes Most played hero
-        most_played_hero = response.text[response.text.find('cloudfront.net/hero/'):].split('/')[2]
-        player[1]['most_played_hero'] = most_played_hero
-        player[1]['portrait'] = 'img/portrait/{}.png'.format(most_played_hero)
+            # Scrapes Most played hero
+            most_played_hero = response.text[response.text.find('akamaihd.net/hero/'):].split('/')[2]
+            player[1]['most_played_hero'] = most_played_hero
+            player[1]['portrait'] = 'img/portrait/{}.png'.format(most_played_hero)
 
-        # Scrapes level
-        player[1]['level'] = int(response.text[response.text.find('<div class="u-vertical-center'):].split('>')[1][:-5])
+            # Scrapes level
+            player[1]['level'] = int(response.text[response.text.find('<div class="u-vertical-center'):].split('>')[1][:-5])
 
-        # Scrapes level frame image URL
-        #player[1]['level_frame_img_url'] = response.text[response.text.find('playerlevelrewards') - 45:].split('(')[1].split(')')[0]
-        player[1]['level_frame_img_url'] = response.text[response.text.find('class="player-level"') - 100:].split('(')[1].split(')')[0]
+            # Scrapes level frame image URL
+            #player[1]['level_frame_img_url'] = response.text[response.text.find('playerlevelrewards') - 45:].split('(')[1].split(')')[0]
+            player[1]['level_frame_img_url'] = response.text[response.text.find('class="player-level"') - 100:].split('(')[1].split(')')[0]
 
-        # Scrapes rank image URL
-        if response.text.find('class="player-rank"') > 0:
-            player[1]['rank_img_url'] = response.text[response.text.find('class="player-rank"') - 100:].split('(')[1].split(')')[0]
+            # Scrapes rank image URL
+            if response.text.find('class="player-rank"') > 0:
+                player[1]['rank_img_url'] = response.text[response.text.find('class="player-rank"') - 100:].split('(')[1].split(')')[0]
 
-        #############################################################################################################
-        ##### Below scraping won't work if the player is new, all the calculation based on scraping goes below ######
+            #############################################################################################################
+            ##### Below scraping won't work if the player is new, all the calculation based on scraping goes below ######
 
-        # Scrapes and calculates winrate
-        if response.text.find('Games Won', 70000) == -1:
-            player[1]['games_won'] = 0
-            player[1]['games_played'] = 0
+            # Scrapes and calculates winrate
+            if response.text.find('Games Won', 70000) == -1:
+                player[1]['games_won'] = 0
+                player[1]['games_played'] = 0
+                redis_store.hset('players_list', player[0], json.dumps(player[1]))
+                continue
+
+            games_won = response.text[response.text.find('Games Won', 70000):].split("</td>")[1][4:].replace(',', '')
+            games_played = response.text[response.text.find('Games Played', 70000):].split("</td>")[1][4:].replace(',', '')
+            player[1]['games_won'] = int(games_won)
+            player[1]['games_played'] = int(games_played)
+            player[1]['winrate'] = '{:.1f}'.format(float(games_won)/float(games_played) * 100)
+
+            # Scrapes and calculates KDA
+            defensive_assists = 0
+            if response.text.find('<td>Defensive Assists</td>') != -1:
+                defensive_assists = response.text[response.text.find('<td>Defensive Assists</td>'):].split('</td>')[1][4:].replace(',', '')
+            offensive_assists = 0
+            if response.text.find('<td>Offensive Assists</td>') != -1:
+                offensive_assists = response.text[response.text.find('<td>Offensive Assists</td>'):].split('</td>')[1][4:].replace(',', '')
+
+
+            kills = response.text[response.text.find('<td>Eliminations</td>'):].split('</td>')[1][4:].replace(',', '')
+            deaths = response.text[response.text.find('<td>Deaths</td>'):].split('</td>')[1][4:].replace(',', '')
+            player[1]['kills'] = kills
+            player[1]['offensive_assists'] = offensive_assists
+            player[1]['defensive_assists'] = defensive_assists
+            player[1]['deaths'] = deaths
+            player[1]['kda'] = '{:.2f}'.format((float(defensive_assists) + float(offensive_assists) + float(kills)) / float(deaths))
+
+            # Stores into Redis
             redis_store.hset('players_list', player[0], json.dumps(player[1]))
-            continue
+        except Exception as e:
+            # log exception and move on
+            pass
 
-        games_won = response.text[response.text.find('Games Won', 70000):].split("</td>")[1][4:].replace(',', '')
-        games_played = response.text[response.text.find('Games Played', 70000):].split("</td>")[1][4:].replace(',', '')
-        player[1]['games_won'] = int(games_won)
-        player[1]['games_played'] = int(games_played)
-        player[1]['winrate'] = '{:.1f}'.format(float(games_won)/float(games_played) * 100)
-
-        # Scrapes and calculates KDA
-        defensive_assists = 0
-        if response.text.find('<td>Defensive Assists</td>') != -1:
-            defensive_assists = response.text[response.text.find('<td>Defensive Assists</td>'):].split('</td>')[1][4:].replace(',', '')
-        offensive_assists = 0
-        if response.text.find('<td>Offensive Assists</td>') != -1:
-            offensive_assists = response.text[response.text.find('<td>Offensive Assists</td>'):].split('</td>')[1][4:].replace(',', '')
-
-
-        kills = response.text[response.text.find('<td>Eliminations</td>'):].split('</td>')[1][4:].replace(',', '')
-        deaths = response.text[response.text.find('<td>Deaths</td>'):].split('</td>')[1][4:].replace(',', '')
-        player[1]['kills'] = kills
-        player[1]['offensive_assists'] = offensive_assists
-        player[1]['defensive_assists'] = defensive_assists
-        player[1]['deaths'] = deaths
-        player[1]['kda'] = '{:.2f}'.format((float(defensive_assists) + float(offensive_assists) + float(kills)) / float(deaths))
-
-        # Stores into Redis
-        redis_store.hset('players_list', player[0], json.dumps(player[1]))
     return 'Refresh successful'
 
 
