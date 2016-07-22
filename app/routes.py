e11c380f603e6d54a52a1f9609658d269a4acbf3
Chris Sim <chriss@airg.com>
2016-07-22 11:14:17 -0700

    sorting by "games_won" instead games_played

--- app/routes.py
+++ app/routes.py
@@ -26,7 +26,7 @@ def main():
             continue
 
     last_updated = redis_store.get('last_updated')
-    data_sorted_by_level = sorted(data, key=lambda player: player[1]['games_played'], reverse=True)
+    data_sorted_by_level = sorted(data, key=lambda player: player[1]['games_won'], reverse=True)
     return render_template('index.html', overwatch_url=OVERWATCH_URL, players=data_sorted_by_level, last_updated=last_updated)
 
 
